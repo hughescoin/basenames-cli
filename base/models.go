673cebc058060f5fefc49d832a7457f9d70524f3
Patrick Hughes <patrick.hughes@coinbase.com>
2024-09-09 11:43:05 -0500

    create a struct to handle latest block response

--- base/models.go
+++ base/models.go
@@ -5,3 +5,9 @@ type Credentials struct {
 	PrivateKey string
 	Address    string
 }
+
+var GetBlockResponse struct {
+	ID      int    `json:"id"`
+	JSONRPC string `json:"jsonrpc"`
+	Result  string `json:"result"`
+}
